83d71a8cb75c2d51c02bb25c1189ede9a69849f3
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-04-25 12:57:34 +0000

    [end]4-4 アプリケーションを完成させよう

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,33 @@
+class ListsController < ApplicationController
+  #------------------------------------------------------new
+  def new
+    # @hello = "Hello"
+    @list = List.new
+  end
+#------------------------------------------------------create
+  def create
+    #1.&2.データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #3.データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #4.トップ画面へリダイレクト
+    redirect_to list_path(list.id)
+  end
+#------------------------------------------------------index
+  def index
+    @lists = List.all
+  end
+#------------------------------------------------------show
+  def show
+    @list = List.find(params[:id])
+  end
+#------------------------------------------------------edit
+  def edit
+  end
+#------------------------------------------------------private
+  private
+    #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#------------------------new
+  get 'lists/new'
+  # get 'top' => 'homes#top'
+#------------------------create
+  post 'lists' => 'lists#create'
+#------------------------index
+  get 'lists' => 'lists#index'
+#------------------------show
+  get 'lists/:id' => 'lists#show', as: 'list'
+#------------------------edit
+  get 'lists/edit'
+  get '/top' => 'homes#top'
+end

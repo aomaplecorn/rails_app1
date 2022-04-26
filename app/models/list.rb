163fc6ac0da34b1272656c0b74f7bde2e05a3c5e
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-04-26 10:38:00 +0000

    4-8 finish

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_one_attached :image
-
+# validates=検証、presence＝存在（true=データ有り、false=無し）
   validates :title, presence: true
   validates :body, presence: true
   validates :image, presence: true

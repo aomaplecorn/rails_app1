cbfabbdb5c113471959bfeaaddf81e86a43f79f5
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-04-26 10:33:34 +0000

    4-8 finish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,11 +7,13 @@ class ListsController < ApplicationController
 #------------------------------------------------------create
   def create
     #1.&2.データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     #3.データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    #4.トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    if @list.save
+    redirect_to list_path(@list.id)
+    else
+    render :new
+    end
   end
 #------------------------------------------------------index
   def index
@@ -25,10 +27,22 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+#------------------------------------------------------destroy
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
 #------------------------------------------------------private
   private
     #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #------------------------new
-  get 'lists/new'
-  # get 'top' => 'homes#top'
-#------------------------create
-  post 'lists' => 'lists#create'
-#------------------------index
-  get 'lists' => 'lists#index'
-#------------------------show
-  get 'lists/:id' => 'lists#show', as: 'list'
-#------------------------edit
-  get 'lists/:id/edit' => 'lists#edit' , as: 'edit_list'
-#------------------------
-  get '/top' => 'homes#top'
+  # get 'lists/new'
+  get 'top' => 'homes#top'
+  resources :lists
+# #------------------------create
+#   post 'lists' => 'lists#create'
+# #------------------------index
+#   get 'lists' => 'lists#index'
+# #------------------------show
+#   get 'lists/:id' => 'lists#show', as: 'list'
+# #------------------------edit
+#   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+#   patch 'lists/:id' => 'lists#update', as: 'update_list'
+# #------------------------destroy
+#   delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+# #------------------------
+#   get '/top' => 'homes#top'
 end
